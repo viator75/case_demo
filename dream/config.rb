7a057d2d2c57efd99132a28d31d60539d4b5e97f
viator75 <tough@fontech.com.tw>
2014-08-04 16:25:00 +0800

    fix

---
+++ dream/config.rb
@@ -0,0 +1,27 @@
+require 'susy'
+# Require any additional compass plugins here.
+
+
+http_path = "/views"
+css_dir = "stylesheets"
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "javascripts"
+
+output_style = :expanded
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+relative_assets = false
+sass_options = {:debug_info=>false}
+
+line_comments = false
+
+preferred_syntax = :sass
+fireapp_coffeescripts_dir = "coffeescripts"
+fireapp_coffeescript_options = {:bare=>false}
+fireapp_livescripts_dir = "livescripts"
+fireapp_livescript_options = {:bare=>false}
+fireapp_minifyjs_on_build = false
+fireapp_always_report_on_build = true
+sass_options = {:debug_info=>false}
